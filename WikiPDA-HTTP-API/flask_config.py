37c6a4c74aa363bb72f08dda0270f32e4dc769ab
Daniel Berg Thomsen <danielbergthomsen@gmail.com>
2021-03-05 23:30:50 +0100

    limit size of POST bodies sent to API

--- WikiPDA-HTTP-API/flask_config.py
+++ WikiPDA-HTTP-API/flask_config.py
@@ -9,4 +9,9 @@ class WikiPDAConfig(object):
     RESTFUL_JSON = {
         # This is required to ensure serialization can happen with numpy types (e.g., np.float32)
         'cls': NumpyEncoder,
+
     }
+
+    # Limits length of incoming articles
+    # Currently set to only handle 15x the longest wikipedia article (1010344206)
+    MAX_CONTENT_LENGTH = 2638714
